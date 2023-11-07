5785c0c7fefd55437412f580314da9afc98c62ff
riccardop <riccardopirani@users.noreply.github.com>
2023-11-07 08:21:37 +0100

    Fix code

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,27 +1,42 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import {
-  CButton,
-  CCard,
-  CCardBody,
-  CCardGroup,
-  CCol,
-  CContainer,
-  CForm,
-  CFormInput,
-  CInputGroup,
-  CInputGroupText,
-  CRow,
-} from '@coreui/react'
+import { useNavigate } from 'react-router-dom'
+import sfondo from 'src/assets/sfondo.png'
+import { CButton, CCol, CFormInput, CInputGroup, CInputGroupText, CRow } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 import { loginUser } from '../../../services/user'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+import { useState, useEffect } from 'react'
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window
+  return {
+    width,
+    height,
+  }
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions())
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return windowDimensions
+}
+
 const Login = () => {
   const navigate = useNavigate()
   let user, password
+  const { height, width } = useWindowDimensions()
   const notify = (message) => toast(message)
 
   const loginfunction = async () => {
@@ -52,74 +67,56 @@ const Login = () => {
     password = val.target.value
   }
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={8}>
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-medium-emphasis">Sign In to your account</p>
-                    <p></p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput
-                        placeholder="Username"
-                        autoComplete="username"
-                        onChange={changeTextUsername}
-                      />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                        onChange={changeTextPassword}
-                      />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs={6}>
-                        <CButton color="primary" className="px-4" onClick={() => loginfunction()}>
-                          Login
-                        </CButton>
-                        <p></p>
-                        <CButton color="primary" className="px-6" onClick={() => followFunction()}>
-                          Segui un defunto
-                        </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Hai dimeticato la password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h3>Non hai un account? Clicca qui sotto per effettuare la registrazione</h3>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Registrati ora
-                      </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-            <ToastContainer />
+    <div
+      style={{
+        backgroundImage: `url(${sfondo})`,
+        height: '100%',
+      }}
+    >
+      <div style={{ height: height, width: width }}>
+        <h1>Login</h1>
+        <p className="text-medium-emphasis">Sign In to your account</p>
+
+        <CInputGroup className="mb-3">
+          <CInputGroupText>
+            <CIcon icon={cilUser} />
+          </CInputGroupText>
+          <CFormInput
+            placeholder="Username"
+            autoComplete="username"
+            onChange={changeTextUsername}
+          />
+        </CInputGroup>
+        <CInputGroup className="mb-4">
+          <CInputGroupText>
+            <CIcon icon={cilLockLocked} />
+          </CInputGroupText>
+          <CFormInput
+            type="password"
+            placeholder="Password"
+            autoComplete="current-password"
+            onChange={changeTextPassword}
+          />
+        </CInputGroup>
+        <CRow>
+          <CCol xs={6}>
+            <CButton color="primary" className="px-4" onClick={() => loginfunction()}>
+              Login
+            </CButton>
+            <p></p>
+            <CButton color="primary" className="px-6" onClick={() => followFunction()}>
+              Segui un defunto
+            </CButton>
+          </CCol>
+          <CCol xs={6} className="text-right">
+            <CButton color="link" className="px-0">
+              Hai dimeticato la password?
+            </CButton>
           </CCol>
         </CRow>
-      </CContainer>
+
+        <ToastContainer />
+      </div>
     </div>
   )
 }
