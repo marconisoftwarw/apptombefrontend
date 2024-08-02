4e9b063c9e494dc5798797bd631e6a95dafd98a2
Riccardo Pirani <riccardo.pirani@reiser.it>
2024-08-02 08:24:56 +0200

    fix code

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,7 +1,8 @@
-const path = 'http://backend.memoryp.org:'
-//var path = 'http://127.0.0.1:'
+var path = 'http://backend.memoryp.org:'
+// var path = 'http://127.0.0.1:';
 export const url = path + '3001'
-export fustion getConfig() {
+
+export function getConfig() {
   const token = localStorage.getItem('token')
   const config = {
     headers: { Authorization: `Bearer ${token}` },
