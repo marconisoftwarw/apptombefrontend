dcf2a8003be3c46f88030e9a2caf4e29651e9626
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-01-11 10:26:03 +0100

    fix code

--- src/views/base/cards/Cards.js
+++ src/views/base/cards/Cards.js
@@ -7,12 +7,12 @@ import {
   CTable,
   CTableBody,
   CTableDataCell,
-  CButton,
   CTableHead,
   CTableHeaderCell,
   CTableRow,
 } from '@coreui/react'
 import { useNavigate } from 'react-router-dom'
+
 import { getList as getListCimitero } from '../../../services/cimitero'
 import { getList as getListDefunti } from '../../../services/defunto'
 import { getList as getTotemList } from '../../../services/totem'
@@ -21,15 +21,12 @@ import Dropdown from 'react-bootstrap/Dropdown'
 const TablesCustom = () => {
   const navigate = useNavigate()
   const [users, setUserList] = useState([])
-
   useEffect(() => {
     var userObjectList = []
     async function fetchData() {
       var defunti = await getListDefunti()
       var cimiteri = await getListCimitero()
       var listaTotem = await getTotemList()
-
-      const urneAssociate = new Set() // Per tenere traccia delle urne già associate
 
       for (var i = 0; i < defunti.length; i++) {
         var totemListValue = []
@@ -39,10 +36,7 @@ const TablesCustom = () => {
         var nome = defunti[i].nome
         var cognome = defunti[i].cognome
         var dataMorte = defunti[i].dataMorte
-        var istemplatevalid = false
         var citta = ''
-
-        // Cerca il cimitero
         for (var i2 = 0; i2 < cimiteri.length; i2++) {
           if (cimiteri[i2].id == defunti[i].idCimitero) {
             citta = cimiteri[i2].citta
@@ -50,19 +44,13 @@ const TablesCustom = () => {
           }
         }
 
-        // Cerca i totem per il cimitero e registra quelli associati
         for (var i3 = 0; i3 < listaTotem.length; i3++) {
           if (idCimitero == listaTotem[i3].idCimitero) {
             isValid = true
             var totemName = listaTotem[i3].id
-            istemplatevalid = listaTotem[i3].istemplatevalid
-            if (istemplatevalid) {
-              urneAssociate.add(totemName) // Aggiungi l'urna alla lista di quelle associate
-            }
             totemListValue.push(totemName)
           }
         }
-
         if (isValid == true) {
           userObjectList.push({
             id: id,
@@ -72,16 +60,9 @@ const TablesCustom = () => {
             idCimitero: idCimitero,
             totemList: totemListValue,
             dataMorte: dataMorte,
-            istemplatevalid: istemplatevalid,
           })
         }
       }
-
-      // Filtra le urne non associate
-      userObjectList = userObjectList.map((user) => ({
-        ...user,
-        totemList: user.totemList.filter((totem) => !urneAssociate.has(totem)),
-      }))
 
       setUserList(userObjectList)
     }
@@ -139,32 +120,20 @@ const TablesCustom = () => {
                 <strong>{item.cimitero}</strong>
               </CTableDataCell>
               <CTableDataCell>
-                {item.istemplatevalid == true ? (
-                  <>
-                    <CTableDataCell className="text-center" key={Math.random()}>
-                      <CButton onClick={() => setlayout(item, item.totemList[0])}>
-                        Modifica Urna
-                      </CButton>
-                    </CTableDataCell>
-                  </>
-                ) : (
-                  <Dropdown style={{ height: 80 }}>
-                    <Dropdown.Toggle
-                      variant="success"
-                      id="dropdown-basic"
-                      style={{ marginTop: '10px' }}
-                    >
-                      Seleziona Urna
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                      {item.totemList.map((value) => (
-                        <Dropdown.Item onClick={() => setlayout(item, value)}>
-                          {value}
-                        </Dropdown.Item>
-                      ))}
-                    </Dropdown.Menu>
-                  </Dropdown>
-                )}
+                <Dropdown style={{ height: 80 }}>
+                  <Dropdown.Toggle
+                    variant="success"
+                    id="dropdown-basic"
+                    style={{ marginTop: '10px' }}
+                  >
+                    Seleziona Urna
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {item.totemList.map((value) => (
+                      <Dropdown.Item onClick={() => setlayout(item, value)}>{value}</Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
               </CTableDataCell>
             </CTableRow>
           ))}
@@ -173,10 +142,8 @@ const TablesCustom = () => {
     </>
   )
 }
-
 const Cards = () => {
   var [cimiteriList, setCimiteriList] = useState([])
-
   useEffect(() => {
     var list = []
     async function fetchData() {
